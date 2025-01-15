4d5e39f73a1e0708102517adfd4cf671c2d1b65d
Renal37 <renalplaycsgo@gmail.com>
2025-01-15 13:47:10 +0300

    test

--- internal/server/config/config.go
+++ internal/server/config/config.go
@@ -9,7 +9,7 @@ import (
 	env "github.com/caarlos0/env/v6"
 )
 
-// ConfigENV contains app settings.
+// ConfigENV содержит настройки приложения.
 type ConfigENV struct {
 	JWTkey             string `json:"jwt_key" env:"JWT_KEY"`
 	Host               string `json:"host" env:"HOST"`
@@ -19,31 +19,31 @@ type ConfigENV struct {
 	MasterKey          string
 }
 
-// GetConfig get app settings.
+// GetConfig получает настройки приложения.
 func GetConfig() (*ConfigENV, error) {
 	var eCfg ConfigENV
 	configPath := "config/server.json"
 
-	flag.StringVar(&eCfg.MasterKey, "mk", "", "master key for encryption keys")
+	flag.StringVar(&eCfg.MasterKey, "mk", "", "мастер-ключ для ключей шифрования")
 	flag.Parse()
 
 	file, err := os.Open(configPath)
 	if err != nil {
-		return nil, fmt.Errorf("failed to open config file: %w", err)
+		return nil, fmt.Errorf("не удалось открыть файл конфигурации: %w", err)
 	}
 
 	decoder := json.NewDecoder(file)
 	if err := decoder.Decode(&eCfg); err != nil {
-		return nil, fmt.Errorf("failed to decode config file: %w", err)
+		return nil, fmt.Errorf("не удалось декодировать файл конфигурации: %w", err)
 	}
 
 	if err := file.Close(); err != nil {
-		return nil, fmt.Errorf("failed close config file: %w", err)
+		return nil, fmt.Errorf("не удалось закрыть файл конфигурации: %w", err)
 	}
 
 	err = env.Parse(&eCfg)
 	if err != nil {
-		return nil, fmt.Errorf("failed parsing environment variables: %w", err)
+		return nil, fmt.Errorf("не удалось разобрать переменные окружения: %w", err)
 	}
 
 	return &eCfg, nil
